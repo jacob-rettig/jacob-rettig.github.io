7e1b4b92a0144c08c58394b95f8abe7ea7347a93
jacob-rettig <36025246+jacob-rettig@users.noreply.github.com>
2018-07-29 14:24:42 -0700

    Add files via upload

---
+++ javascript_puzzle1.js
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+
+<script src="https://d3js.org/d3.v4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+
+<html>
+
+<body>
+
+    <div id="decoy-svg-container">
+    <svg id="decoy_viz" width="0" height="0"></svg>
+    </div>
+
+    <div id="svg-container">
+    <svg id="viz" width="720" height="405"></svg>
+    </div>
+
+<script>
+
+    var width = 720;
+    var height = 405;
+
+</script>
+
+<script>
+var data = 
+[
+    10,
+    5,
+    5,
+    8,
+    9,
+    3,
+    2
+]; 
